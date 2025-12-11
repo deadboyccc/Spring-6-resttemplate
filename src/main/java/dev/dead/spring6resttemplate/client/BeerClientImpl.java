cac1f17e3bdd5322330dc6ee1d6db66190af142d
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-11 03:35:31 +0300

    Refactor BeerClientImpl to use constants for base URL and path in API calls

--- src/main/java/dev/dead/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/dev/dead/spring6resttemplate/client/BeerClientImpl.java
@@ -9,17 +9,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
+    private static final String BASE_URL = "http://localhost:8080";
+    private final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/v1/beer",
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH,
                 String.class);
+        ResponseEntity<Map> responseEntityMap = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+        assert responseEntityMap.getBody() != null;
+        log.debug("Response Entity Map: {}", responseEntityMap.getBody()
+                .size());
+
 
         log.info("Status Code: {}", responseEntity.getStatusCode());
         log.debug("Response Body: {}", responseEntity.getBody());
